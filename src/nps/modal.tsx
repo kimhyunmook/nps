298cb17f88db7dd0efaad4a8f3b03ad9038fb75a
Kimhyunmook <gusanr4200@naver.com>
2025-04-23 22:37:20 +0900

    0.1.15

--- src/nps/modal.tsx
+++ src/nps/modal.tsx
@@ -38,90 +38,91 @@ export default function NpsModal({
 
   const propsValue = Object.entries(component?.props ?? {});
   const cpName = component.name + " (Component)";
-
   return (
-    <div className={`${npsm.infomation} ${isOpen && npsm.display}`}>
-      <div className={npsm.top}>
-        <h2 className={npsm.name}>{!!component.name ? cpName : "Element"}</h2>
-        <a className={npsm.closeBtn} onClick={close}></a>
-      </div>
-      <DisplayName component={component} />
-      <div className={npsm.elements}>{component.element}</div>
-      {component.vscUrl && <CodeBtn url={component.vscUrl} />}
-      <div className={npsm.lists}>
-        <h2 className={npsm.title}>Props</h2>
-        <ul>
-          {propsValue.length > 0 ? (
-            propsValue.map((v, index) => {
-              let value = typeof v[1] as React.ReactNode;
-              if (v[1] === null) value = "null";
-              switch (value) {
-                case "function":
-                  value = "void";
-                  break;
+    <Draggalbe style={isOpen ? { bottom: 0 } : {}}>
+      <div className={`${npsm.infomation}`}>
+        <div className={npsm.top}>
+          <h2 className={npsm.name}>{!!component.name ? cpName : "Element"}</h2>
+          <a className={npsm.closeBtn} onClick={close}></a>
+        </div>
+        <DisplayName component={component} />
+        <div className={npsm.elements}>{component.element}</div>
+        {component.vscUrl && <CodeBtn url={component.vscUrl} />}
+        <div className={npsm.lists}>
+          <h2 className={npsm.title}>Props</h2>
+          <ul>
+            {propsValue.length > 0 ? (
+              propsValue.map((v, index) => {
+                let value = typeof v[1] as React.ReactNode;
+                if (v[1] === null) value = "null";
+                switch (value) {
+                  case "function":
+                    value = "void";
+                    break;
 
-                case "object":
-                  // 배열인 경우
-                  if (Array.isArray(v[1])) {
-                    const overlap = v[1].map((v) => typeof v);
-                    const setOverlap = new Set(overlap);
-                    const arr = [...setOverlap];
+                  case "object":
+                    // 배열인 경우
+                    if (Array.isArray(v[1])) {
+                      const overlap = v[1].map((v) => typeof v);
+                      const setOverlap = new Set(overlap);
+                      const arr = [...setOverlap];
 
-                    value = arr.reduce((a, c, i) => {
-                      a += c;
-                      if (i !== arr.length - 1) a += "|";
-                      else a += "[ ]";
-                      return a;
-                    }, "");
-                  } else {
-                    // 객체인 경우
-                    const arr = Object.entries(v[1]);
+                      value = arr.reduce((a, c, i) => {
+                        a += c;
+                        if (i !== arr.length - 1) a += "|";
+                        else a += "[ ]";
+                        return a;
+                      }, "");
+                    } else {
+                      // 객체인 경우
+                      const arr = Object.entries(v[1]);
 
-                    const reactChk = arr[0].find((x) => x === "$$typeof");
-                    if (!!reactChk) {
-                      value = "React.ReactNode";
-                      return (
-                        <li className={""} key={index}>
-                          <h3 className={npsm.key}>{v[0]}</h3>
-                          <p className={npsm.value}>{value}</p>
-                        </li>
-                      );
+                      const reactChk = arr[0].find((x) => x === "$$typeof");
+                      if (!!reactChk) {
+                        value = "React.ReactNode";
+                        return (
+                          <li className={""} key={index}>
+                            <h3 className={npsm.key}>{v[0]}</h3>
+                            <p className={npsm.value}>{value}</p>
+                          </li>
+                        );
+                      }
+                      value = arr.map((v2, i) => {
+                        return (
+                          <span key={v2[0]} className={npsm.ob}>
+                            {i === 0 && "{"}
+                            <span className={npsm.key}>{v2[0]}</span>
+                            <span className={npsm.value}>{typeof v2[1]}</span>
+                            {i === arr.length - 1 && " }"}
+                          </span>
+                        );
+                      });
                     }
-                    value = arr.map((v2, i) => {
-                      return (
-                        <span key={v2[0]} className={npsm.ob}>
-                          {i === 0 && "{"}
-                          <span className={npsm.key}>{v2[0]}</span>
-                          <span className={npsm.value}>{typeof v2[1]}</span>
-                          {i === arr.length - 1 && " }"}
-                        </span>
-                      );
-                    });
-                  }
-                  break;
-              }
-              const key = v[0];
-              if (key.length > keyMaxLength) setKML(key.length);
+                    break;
+                }
+                const key = v[0];
+                if (key.length > keyMaxLength) setKML(key.length);
 
-              return (
-                <li key={index}>
-                  <h3
-                    className={npsm.key}
-                    style={{ width: `${keyMaxLength - 1}ch` }}
-                  >
-                    {key}
-                  </h3>
-                  <p className={npsm.value}>{value}</p>
-                </li>
-              );
-            })
-          ) : (
-            <li className={npsm.noProps}>
-              <p>Props가 없어요</p>
-            </li>
-          )}
-        </ul>
+                return (
+                  <li key={index}>
+                    <h3
+                      className={npsm.key}
+                      style={{ width: `${keyMaxLength - 1}ch` }}
+                    >
+                      {key}
+                    </h3>
+                    <p className={npsm.value}>{value}</p>
+                  </li>
+                );
+              })
+            ) : (
+              <li className={npsm.noProps}>
+                <p>Props가 없어요</p>
+              </li>
+            )}
+          </ul>
+        </div>
       </div>
-    </div>
+    </Draggalbe>
   );
 }
